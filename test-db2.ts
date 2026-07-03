0e76d45095aa6bad6fc872b25a57bf5ce792f507
Alanda-admirable <pichet.mekim@gmail.com>
2026-07-03 19:40:52 +0700

    fix: remove temp files breaking build

--- test-db2.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-async function main() {
-  const schedule = await prisma.schedule.findUnique({
-    where: { id: '4653b8be-e326-450d-ace9-2f10c8277d52' }
-  })
-  console.log(JSON.stringify(schedule?.mission))
-}
-main().catch(console.error).finally(() => prisma.$disconnect())
--- test-render2.js
+++
@@ -1,7 +0,0 @@
-const renderText = (text) => {
-    let formatted = text;
-    formatted = formatted.replace(/[ \t]+\(([^)]+)\)/g, '\u00A0($1)');
-    return formatted.split('\n');
-};
-const res = renderText("ประจำปี 2569 \n(Queen's Cup Thailand Championship)");
-console.log(JSON.stringify(res));
